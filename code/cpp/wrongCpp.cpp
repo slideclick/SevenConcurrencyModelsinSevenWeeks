e860d33686295e0ba77254d43179bb3b30158413
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-04-02 11:21:25 +0800

    a thread problem on vs2013 which don't support 'magic static'

---
+++ code/cpp/wrongCpp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+// should not print Oh while it will
+// VS2013 will print Oh
+//http://www.ituring.com.cn/article/194454
+
+DWORD WINAPI ThreadProc(LPVOID lpParameter)
+{
+    static std::string x = std::string(10000000, 'e');
+    if (x.length() == 0) {
+        std::cout << "Oh!" << std::endl;
+    }
+    else {
+        std::cout << x.length() << std::endl;
+    }
+
+    return 0;
+}
+
+int main()
+{
+    CreateThread(NULL, 0, ThreadProc, NULL, 0, NULL);
+    CreateThread(NULL, 0, ThreadProc, NULL, 0, NULL);
+    Sleep(100);
+    return 0;
+}
